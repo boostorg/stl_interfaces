41d3c619e8f97e642d192c89246c069d9214f4cc
Zach Laine <whatwasthataddress@gmail.com>
2019-08-04 15:55:41 -0500

    Add copyright headers to sources.

--- include/boost/iterator_facade/iterator_facade.hpp
+++ include/boost/iterator_facade/iterator_facade.hpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2019 T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_ITERATOR_FACADE_ITERATOR_FACADE_HPP
 #define BOOST_ITERATOR_FACADE_ITERATOR_FACADE_HPP
 
--- test/bidirectional.cpp
+++ test/bidirectional.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2019 T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/iterator_facade/iterator_facade.hpp>
 
 #include <gtest/gtest.h>
