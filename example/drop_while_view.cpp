1f7b6b886bccaedf0fea3792ffaafe221fc1c11a
Zach Laine <whatwasthataddress@gmail.com>
2019-08-13 22:59:09 -0500

    Round out view_interface docs.

--- example/drop_while_view.cpp
+++ example/drop_while_view.cpp
@@ -13,9 +13,8 @@
 
 //[ all_view
 // A subrange is simply an iterator-sentinel pair.  This one is a bit simpler
-// than the one in std::ranges, but not by a lot.  Note that we're inheriting
-// from view_interface, which gives us more operations than what we've written
-// here.
+// than the one in std::ranges; its missing a bunch of constructors, prev(),
+// next(), and advance().
 template<typename Iterator, typename Sentinel>
 struct subrange
     : boost::stl_interfaces::view_interface<subrange<Iterator, Sentinel>>
@@ -32,7 +31,7 @@ private:
 };
 
 // std::view::all() returns one of several types, depending onwhat you pass
-// it.  Here, we'te keeping it simple; all() always returns a subrange.
+// it.  Here, we're keeping it simple; all() always returns a subrange.
 template<typename Range>
 auto all(Range && range)
 {
@@ -47,12 +46,12 @@ using all_view = decltype(all(std::declval<Range>()));
 
 //[ drop_while_view_template
 
-// Here we finally get to drop_while_view itself.  Perhaps its clear now why
-// we defined subrange, all, etc. above.  drop_while_view contains a view data
-// member.  If we just took any old range that was passed to drop_while_view's
-// constructor, we'd copy the range itself, which may be a std::vector.  So,
-// we want to make a view out of whatever Range we're given so that this copy
-// of an owning range does not happen.
+// Perhaps its clear now why we defined subrange, all(), etc. above.
+// drop_while_view contains a view data member.  If we just took any old range
+// that was passed to drop_while_view's constructor, we'd copy the range
+// itself, which may be a std::vector.  So, we want to make a view out of
+// whatever Range we're given so that this copy of an owning range does not
+// happen.
 template<typename Range, typename Pred>
 struct drop_while_view
     : boost::stl_interfaces::view_interface<drop_while_view<Range, Pred>>
@@ -70,9 +69,12 @@ struct drop_while_view
     constexpr Pred const & pred() const noexcept { return pred_; }
 
     // A more robust implementation should probably cache the value computed
-    // by this function, so that aubsequent calls are optimized.
+    // by this function, so that aubsequent calls can just return the cached
+    // iterator.
     constexpr auto begin()
     {
+        // We're forced to write this out as a raw loop, since no
+        // std::-namespace algorithms accept a sentinel.
         auto first = base_.begin();
         auto const last = base_.end();
         for (; first != last; ++first) {
@@ -101,30 +103,33 @@ auto make_drop_while_view(Range & base, Pred pred)
 
 int main()
 {
-    //[ all_usage
+    //[ drop_while_view_usage
     std::vector<int> const ints = {2, 4, 3, 4, 5, 6};
 
-    // All returns a subrange, which is a view type containing ints.begin()
+    // all() returns a subrange, which is a view type containing ints.begin()
     // and ints.end().
     auto all_ints = all(ints);
 
-    // This works using just the explicitly implemented members of subrange,
-    // begin() and end().
+    // This works using just the used-defined members of subrange: begin() and
+    // end().
     assert(
         std::equal(all_ints.begin(), all_ints.end(), ints.begin(), ints.end()));
 
     // These are available because subrange is derived from view_interface.
     assert(all_ints[2] == 3);
     assert(all_ints.size() == 6u);
-    //]
 
-    //[ drop_while_simple_usage
     auto even = [](int x) { return x % 2 == 0; };
     auto ints_after_even_prefix = make_drop_while_view(ints, even);
+
+    // Available via begin()/end()...
     assert(std::equal(
         ints_after_even_prefix.begin(),
         ints_after_even_prefix.end(),
         ints.begin() + 2,
         ints.end()));
+
+    // ... and via view_interface.
+    assert(!ints_after_even_prefix.empty());
     //]
 }
