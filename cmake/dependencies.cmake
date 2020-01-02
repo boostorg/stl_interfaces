9414d1a01094eff8e299b1dc84f57945284f9c0a
Zach Laine <whatwasthataddress@gmail.com>
2020-01-01 18:33:58 -0600

    Add perf note about proxy_arrow_result to docs.
    Fixes #30.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -32,19 +32,6 @@ endif ()
 
 
 ###############################################################################
-# GoogleTest
-###############################################################################
-add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.8.0)
-target_include_directories(gtest      INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.8.0/googletest/include)
-target_include_directories(gtest_main INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.8.0/googletest/include)
-
-
-###############################################################################
-# Google Benchmark
-###############################################################################
-#add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark-v1.2.0)
-
-###############################################################################
 # cmcstl2, and experimental implementation of P0896R4 "The One Ranges Proposal"
 ###############################################################################
 if (EXISTS "${CMAKE_SOURCE_DIR}/cmcstl2")
